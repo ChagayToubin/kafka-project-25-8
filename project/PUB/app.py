88e05dfc0a51c878b9f4f1c1847996175079a786
chagay <chagaytoubin@gmail.com>
2025-08-26 12:21:26 +0300

    start connect mongo

--- project/PUB/app.py
+++ project/PUB/app.py
@@ -1,17 +1,29 @@
-from fastapi import FastAPI
 from project.PUB.data import Data
+from project.PUB.Publishe import Producer
+#
+# from data import Data
+# from Publishe import Producer
+
+from fastapi import FastAPI
+
+from fastapi.responses import HTMLResponse
+
 app = FastAPI()
 
-data=Data()
-
-@app.get("/")
-def root():
-    return  {"data1":dict(data.data_loader_interesting())}
-
-@app.get("/not_interesting")
-def not_interesting():
-    return {"ds":"ds"}
+@app.get("/", response_class=HTMLResponse)
+def home():
+    data=Data()
+    P=Producer()
+    return """
+    <h1>בחר פעולה</h1>
+    <button onclick="location.href='/interesting'">interesting</button>
+    <button onclick="location.href='/not_interesting'">not interesting</button>
+    """
 
 @app.get("/interesting")
-def interesting():
-    return {"ds":"ds"}
+def do_interesting():
+    return {"message": "הופעלה פעולה: מעניין"}
+
+@app.get("/not_interesting")
+def do_not_interesting():
+    return {"message": "הופעלה פעולה: לא מעניין"}
